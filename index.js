2638b282311c6b9ea29c26fff65be7c08f293faf
Anton Golub <mailbox@antongolub.ru>
2021-03-24 19:01:26 +0300

    style: configure & apply eslint

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
+'use strict';
+
 const getNpmPkgVersion = function (packageName, { registry = '', timeout = null } = {}) {
-    try {
-        if (/[`$&{}[;|]/g.test(packageName) || /[`$&{}[;|]/g.test(registry)) {
-            return null
-        }
-        let version;
-
-        const config = {
-            stdio: ['pipe', 'pipe', 'ignore']
-        };
-
-        if (timeout) {
-            config.timeout = timeout;
-        }
-
-        if (registry) {
-            version = require('child_process').execSync(`npm view ${packageName} version --registry ${registry}`, config);
-        } else {
-            version = require('child_process').execSync(`npm view ${packageName} version`, config);
-        }
-
-        if (version) {
-            return version.toString().trim().replace(/^\n*|\n*$/g, '');
-        } else {
-            return null;
-        }
-
-    } catch(err) {
-        return null;
+  try {
+    if (/[`$&{}[;|]/g.test(packageName) || /[`$&{}[;|]/g.test(registry)) {
+      return null;
     }
-}
+    let version;
 
-getNpmPkgVersion.getNpmPkgVersion = getNpmPkgVersion // Named export
+    const config = {
+      stdio: ['pipe', 'pipe', 'ignore']
+    };
 
-module.exports = getNpmPkgVersion
+    if (timeout) {
+      config.timeout = timeout;
+    }
+
+    if (registry) {
+      version = require('child_process').execSync(`npm view ${packageName} version --registry ${registry}`, config);
+    } else {
+      version = require('child_process').execSync(`npm view ${packageName} version`, config);
+    }
+
+    if (version) {
+      return version.toString().trim().replace(/^\n*|\n*$/g, '');
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+};
+
+getNpmPkgVersion.getNpmPkgVersion = getNpmPkgVersion; // Named export
+
+module.exports = getNpmPkgVersion;
